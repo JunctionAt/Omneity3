b335e5f5d5d5991adb4babeb7a55e5db4625e016
Tom Powell <zifnab@zifnab06.net>
2014-04-12 17:52:56 -0600

    Copy default config on first run

--- src/main/java/at/junction/omneity3/Omneity3.java
+++ src/main/java/at/junction/omneity3/Omneity3.java
@@ -8,10 +8,18 @@ import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
 
+import java.io.File;
+
 public class Omneity3 extends JavaPlugin{
     public Configuration config;
     @Override
     public void onEnable(){
+        File cfile = new File(getDataFolder(), "config.yml");
+        if (!cfile.exists()) {
+            getConfig().options().copyDefaults(true);
+            saveConfig();
+        }
+
         config = new Configuration(this);
         config.load();
         getServer().getPluginManager().registerEvents(new Omneity3Listener(this), this);
