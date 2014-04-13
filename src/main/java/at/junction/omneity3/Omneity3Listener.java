e4f988b9d96d086b56388915e3029a0039b0eeab
Tom Powell <zifnab@zifnab06.net>
2014-04-12 20:21:25 -0600

    Buff chance of finding saddles & nametags when fishing

--- src/main/java/at/junction/omneity3/Omneity3Listener.java
+++ src/main/java/at/junction/omneity3/Omneity3Listener.java
@@ -1,13 +1,18 @@
 package at.junction.omneity3;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
 
 public class Omneity3Listener implements Listener {
     Omneity3 plugin;
@@ -39,4 +44,19 @@ public class Omneity3Listener implements Listener {
         }
     }
 
+    @EventHandler
+    void onPlayerFishEvent(PlayerFishEvent event){
+        if (event.getState() == PlayerFishEvent.State.CAUGHT_ENTITY || event.getState() == PlayerFishEvent.State.CAUGHT_FISH){
+            if (new Random().nextInt(100) < 10){
+                ItemStack item;
+
+                if (new Random().nextInt(2) == 1){
+                    item = new ItemStack(Material.NAME_TAG);
+                } else {
+                    item = new ItemStack(Material.SADDLE);
+                }
+                event.getPlayer().getLocation().getWorld().dropItem(event.getPlayer().getLocation(), item);
+            }
+        }
+    }
 }
