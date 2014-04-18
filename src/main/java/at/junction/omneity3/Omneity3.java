7de40d58d689e80b095c2006c9d0cd71adb5b257
Tom Powell <zifnab@zifnab06.net>
2014-04-17 20:05:21 -0600

    Add /world <world> [<x> <y> <z>] command to mod toolkit: teleports you to <world> with optional coordinates. No coordinates = world's spawn.

--- src/main/java/at/junction/omneity3/Omneity3.java
+++ src/main/java/at/junction/omneity3/Omneity3.java
@@ -1,7 +1,9 @@
 package at.junction.omneity3;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -10,10 +12,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 
-public class Omneity3 extends JavaPlugin{
+public class Omneity3 extends JavaPlugin {
     public Configuration config;
+
     @Override
-    public void onEnable(){
+    public void onEnable() {
         File cfile = new File(getDataFolder(), "config.yml");
         if (!cfile.exists()) {
             getConfig().options().copyDefaults(true);
@@ -28,26 +31,46 @@ public class Omneity3 extends JavaPlugin{
 
 
     @Override
-    public void onDisable(){
+    public void onDisable() {
 
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String name, String[] args) {
-        if (command.getName().equals("spawn")){
-            if (sender instanceof Player){
-                ((Player)sender).teleport(config.spawn.LOCATION);
+        if (command.getName().equals("spawn")) {
+            if (sender instanceof Player) {
+                ((Player) sender).teleport(config.spawn.LOCATION);
                 sender.sendMessage(ChatColor.RED + "Wooosh!");
             } else {
                 sender.sendMessage(ChatColor.RED + "Only usable by players, sorry!");
             }
-        } else if (command.getName().equalsIgnoreCase("o3-reload")){
+        } else if (command.getName().equalsIgnoreCase("o3-reload")) {
             config.reload();
+        } else if (command.getName().equalsIgnoreCase("world")) {
+            if (args.length == 0) return false;
+            if (!(args.length == 4 || args.length == 1)) return false;
+            if (!(sender instanceof Player)){
+                sender.sendMessage("Only usable by players");
+                return false;
+            }
+            World world = getServer().getWorld(args[0]);
+            if (world == null){
+                sender.sendMessage("This world doesn't exist. Try 'world', 'world_nether', or 'world_the_end'");
+                return true;
+            }
+
+            if (args.length == 4) {
+                ((Player)sender).teleport(new Location(world, Double.parseDouble(args[1]), Double.parseDouble(args[2]), Double.parseDouble(args[3])));
+            } else {
+                ((Player)sender).teleport(world.getSpawnLocation());
+            }
+
+
         }
         return true;
     }
 
-    public void loadRecipes(){
+    public void loadRecipes() {
         String[] bardingLayout = {"  A", "ASA", "AAA"};
 
         //diamondBarding
