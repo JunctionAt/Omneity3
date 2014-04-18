3ec3c910e1d0a93c3136f18dc7dfd414869deebd
Tom Powell <zifnab@zifnab06.net>
2014-04-17 19:45:15 -0600

    Try to clean up mob lag some - remove unnamed unarmored mobs on chunk unload.

--- src/main/java/at/junction/omneity3/Omneity3Listener.java
+++ src/main/java/at/junction/omneity3/Omneity3Listener.java
@@ -1,58 +1,95 @@
 package at.junction.omneity3;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.*;
+import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
 
 public class Omneity3Listener implements Listener {
     Omneity3 plugin;
-    public Omneity3Listener(Omneity3 plugin){
+
+    public Omneity3Listener(Omneity3 plugin) {
         this.plugin = plugin;
     }
+
     @EventHandler
-    void onPlayerJoinEvent(PlayerJoinEvent event){
+    void onPlayerJoinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        if (!player.hasPlayedBefore()){
+        if (!player.hasPlayedBefore()) {
             player.teleport(plugin.config.spawn.LOCATION);
-            if (plugin.config.spawn.FIRST_JOIN_ENABLED){
+            if (plugin.config.spawn.FIRST_JOIN_ENABLED) {
                 plugin.getServer().broadcastMessage(String.format(plugin.config.spawn.FIRST_JOIN_MESSAGE, ChatColor.DARK_PURPLE, player.getName()));
             }
         }
     }
 
     @EventHandler
-    void onPlayerPortalEvent(PlayerPortalEvent event){
-        if (event.getFrom().getWorld().getEnvironment() == World.Environment.THE_END && !plugin.config.spawn.BED_SPAWN_ENABLED){
+    void onPlayerPortalEvent(PlayerPortalEvent event) {
+        if (event.getFrom().getWorld().getEnvironment() == World.Environment.THE_END && !plugin.config.spawn.BED_SPAWN_ENABLED) {
             event.setTo(plugin.config.spawn.LOCATION);
         }
     }
 
     @EventHandler
-    void onPlayerRespawnEvent(PlayerRespawnEvent event){
-        if (event.isBedSpawn() && !plugin.config.spawn.BED_SPAWN_ENABLED){
+    void onPlayerRespawnEvent(PlayerRespawnEvent event) {
+        if (event.isBedSpawn() && !plugin.config.spawn.BED_SPAWN_ENABLED) {
             event.setRespawnLocation(plugin.config.spawn.LOCATION);
         }
     }
 
     @EventHandler
-    void onPlayerFishEvent(PlayerFishEvent event){
-        if (event.getState() == PlayerFishEvent.State.CAUGHT_ENTITY || event.getState() == PlayerFishEvent.State.CAUGHT_FISH){
-            if (new Random().nextInt(100) < 10){
+    void onPlayerFishEvent(PlayerFishEvent event) {
+        if (event.getState() == PlayerFishEvent.State.CAUGHT_ENTITY || event.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
+            if (new Random().nextInt(100) < 10) {
                 ItemStack item;
 
-                if (new Random().nextInt(2) == 1){
+                if (new Random().nextInt(2) == 1) {
                     item = new ItemStack(Material.NAME_TAG);
                 } else {
                     item = new ItemStack(Material.SADDLE);
                 }
                 event.getPlayer().getLocation().getWorld().dropItem(event.getPlayer().getLocation(), item);
+            }
+        }
+    }
+    //When a chunk unloads, remove its monsters, excluding Withers
+    //EnderDragon is NOT a monster, it is a ComplexLivingEntity (LEAVE ENDER ALONE)
+    //Hoping this cuts down on lag?
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onChunkUnload(ChunkUnloadEvent event) {
+        Chunk chunk = event.getChunk();
+        for (Entity entity : chunk.getEntities()) {
+            if (!entity.isDead() && entity instanceof Monster) {
+                Monster monster = (Monster) entity;
+
+                Boolean remove = true;
+
+                if (monster.getCustomName() != null){
+                    remove = false;
+                }
+                if ( monster.getType() == EntityType.WITHER) {
+                    remove = false;
+                }
+                for (ItemStack item : monster.getEquipment().getArmorContents()) {
+                    if (item != null) {
+                        remove = false;
+                        break;
+                    }
+                }
+                if (remove)
+                    monster.remove();
             }
         }
     }
